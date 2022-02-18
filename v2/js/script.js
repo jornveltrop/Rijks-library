b0aae7b5aac92cbb2ea86775478a75223430e5bc
jornveltrop <jorn1veltrop@gmail.com>
2022-02-18 18:47:21 +0100

    changed structure

---
+++ v2/js/script.js
@@ -0,0 +1,54 @@
+const sectionData = document.querySelector("section");
+const aantalResults = 100;
+const imgSize = 600;
+
+function getAndRenderData () {
+    
+    //Verwijder huidige kunstwerken
+    const articleElements = document.querySelectorAll("section > article");
+    for (var i = 0, l = articleElements.length; i < l; i++) {
+        articleElements[i].remove();
+    }
+    
+    //Titel vullen met zoekterm
+    const h1 = document.querySelector("h1");
+    let inputField = document.querySelector("input").value;
+    h1.innerHTML = "Gezocht naar: " + inputField; 
+
+    //In geval geen input zoekbalk, laat uitgelicht zien
+    if (inputField == 0) {
+        inputField = "";
+        h1.innerHTML = "Uitgelicht"; 
+    }
+
+    //Stel URL in (100)
+    const getURL = "https://www.rijksmuseum.nl/api/nl/collection?key=AbH3UnTw&q=" + inputField + "&ps=" + aantalResults;
+    
+    //Haal data op
+    fetch(getURL).then(response => response.json())
+    .then(data => {
+        
+        //Pak kunst objecten
+        const objects = data.artObjects;
+        console.log(objects);
+
+        //Check of er 0 resultaten zijn
+        if (objects.length == 0) {
+            h1.innerHTML = "'" + inputField + "'" + " gaf helaas geen resultaten."; 
+        } 
+        else {
+            //Genereer HTML blokken voor elk resultaat
+            objects.forEach(number => {
+                let scaledImg = number.webImage.url + imgSize;
+                sectionData.insertAdjacentHTML("beforeend",`
+                    <article>
+                        <h2>${number.title}</h2>
+                        <img src="${scaledImg}">
+                    </article
+                `)
+            });
+        }
+    }).catch(error => console.log(error))
+}
+
+getAndRenderData();
