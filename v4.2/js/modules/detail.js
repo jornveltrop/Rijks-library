4407b174ef714727233bdf422a10aa4a25457d14
jornveltrop <jorn1veltrop@gmail.com>
2022-03-04 14:57:27 +0100

    Added UI styling 2

--- v4.2/js/modules/detail.js
+++ v4.2/js/modules/detail.js
@@ -2,10 +2,10 @@ import { openAside } from './ui.js'
 
 const imgSize = 2000;
 const apiKey = "AbH3UnTw";
-const aside = document.querySelector("aside");
+const asideSection = document.querySelector("aside > section");
 const h2Aside = document.querySelector("aside h2");
 const h3Aside = document.querySelector("aside h3");
-const imgAside = document.querySelector("aside img");
+const imgAside = document.querySelector("aside > section img");
 const pAside = document.querySelector("aside p");
 const arrowIcon = document.querySelector(".arrowIcon");
 
@@ -40,7 +40,7 @@ export async function renderDetailData(detailData) {
     pAside.innerHTML = objectData.label.description;
 
     setTimeout(function(){
-        let verschilScroll = aside.scrollHeight - aside.clientHeight;
+        let verschilScroll = asideSection.scrollHeight - asideSection.clientHeight;
         if (verschilScroll > 30) {
             console.log("check");
             arrowIcon.classList.add("arrowVisible")
